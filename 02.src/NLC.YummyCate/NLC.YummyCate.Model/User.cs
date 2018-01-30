11c337aa197fa20eef91e20fa12f660736969a26
lixue <lixue@aecg.com.cn>
2018-01-30 18:23:45 +0800

    model属性代码

--- 02.src/NLC.YummyCate/NLC.YummyCate.Model/User.cs
+++ 02.src/NLC.YummyCate/NLC.YummyCate.Model/User.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace NLC.YummyCate.Model
 {
-	class User
+	public class User
 	{
+		/// <summary>
+		/// 用户ID
+		/// </summary>
+		public int UserID { get; set; }
+
+		/// <summary>
+		/// 密码
+		/// </summary>
+		public string Password { get; set; }
+
+		/// <summary>
+		/// 用户名
+		/// </summary>
+		public string UserName { get; set; }
+
+		/// <summary>
+		/// 权限编号
+		/// </summary>
+		public int AuthorityNumber { get; set; }
+
+		/// <summary>
+		/// 权限名称
+		/// </summary>
+		public string AuthorityName { get; set; }
+
+		/// <summary>
+		/// 员工姓名
+		/// </summary>
+		public string StaffName { get; set; }
+
+		/// <summary>
+		/// 部门名称
+		/// </summary>
+		public string DepatementName { get; set; }
+
+		/// <summary>
+		/// 部门编号
+		/// </summary>
+		public int StaffID { get; set; }
+
+
 	}
 }
