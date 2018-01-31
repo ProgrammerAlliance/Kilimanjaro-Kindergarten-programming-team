892ab39c3c1c1b6d6439223924d9a9a8b5cc4113
fczmm <2807230217@qq.com>
2018-01-31 16:23:45 +0800

    1

--- 02.src/NLC.YummyCate/NLC.YummyCate.SqlServerDAL/SqlSeverUserDAL.cs
+++ 02.src/NLC.YummyCate/NLC.YummyCate.SqlServerDAL/SqlSeverUserDAL.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLC.YummyCate.DBUtility;
+using System.Data.SqlClient;
 
 namespace NLC.YummyCate.SqlServerDAL
 {
-    class SqlSeverUserDal
+  public  class SqlSeverUserDal
     {
+        string connStr = "Data";
+        SqlConnection conn = new SqlConnection();
+        string selectsql = "select * from StaffInformation";
+        SqlCommand cmd = new SqlCommand(selectsql, conn);
+
+
+
     }
 }
