7c724df8780452094827a5f58c4c46d688250fd6
fczmm <2807230217@qq.com>
2018-02-10 23:37:56 +0800

    修改

---
+++ 02.src/NLC.YummyCate/NLC.YummyCate.Model/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLC.YummyCate.Model
+{
+  public   class UserInfo
+    {
+        public string UserName { get; set; }
+        public string Meno { get; set; }
+        public string StaffName { get; set; }
+        public string Datetime { get; set; }
+    }
+}
